2ad4eb7e0231c629d1c04e7e989f1f34f4ee5906
itswcg <itswcg@gmail.com>
2020-02-15 15:12:25 +0800

    Add grpcrunserver command

--- grpc_framework/management/commands/grpcrunserver.py
+++ grpc_framework/management/commands/grpcrunserver.py
@@ -7,10 +7,9 @@ from datetime import datetime
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-from django.core.servers.basehttp import (
-    WSGIServer, get_internal_wsgi_application, run,
-)
 from django.utils import autoreload
+
+from grpc_framework.server import GrpcServer
 
 naiveip_re = re.compile(r"""^(?:
 (?P<addr>
@@ -21,56 +20,34 @@ naiveip_re = re.compile(r"""^(?:
 
 
 class Command(BaseCommand):
-    help = "Starts a lightweight Web server for development."
+    help = "Starts a GRPC server"
 
     # Validation is called explicitly each time the server is reloaded.
     requires_system_checks = False
     stealth_options = ('shutdown_message',)
 
-    default_addr = '127.0.0.1'
-    default_addr_ipv6 = '::1'
-    default_port = '8001'
-    protocol = 'http'
-    server_cls = WSGIServer
+    default_addr = '0.0.0.0'
+    default_port = '50051'
+    protocol = 'grpc'
+    server_cls = GrpcServer
 
     def add_arguments(self, parser):
         parser.add_argument(
             'addrport', nargs='?',
             help='Optional port number, or ipaddr:port'
         )
+
         parser.add_argument(
-            '--ipv6', '-6', action='store_true', dest='use_ipv6',
-            help='Tells Django to use an IPv6 address.',
+            "--workers", dest="max_workers",
+            help="Number of maximum worker threads"
         )
-        parser.add_argument(
-            '--nothreading', action='store_false', dest='use_threading',
-            help='Tells Django to NOT use threading.',
-        )
+
         parser.add_argument(
             '--noreload', action='store_false', dest='use_reloader',
             help='Tells Django to NOT use the auto-reloader.',
         )
 
-    def execute(self, *args, **options):
-        if options['no_color']:
-            # We rely on the environment because it's currently the only
-            # way to reach WSGIRequestHandler. This seems an acceptable
-            # compromise considering `runserver` runs indefinitely.
-            os.environ["DJANGO_COLORS"] = "nocolor"
-        super().execute(*args, **options)
-
-    def get_handler(self, *args, **options):
-        """Return the default WSGI handler for the runner."""
-        return get_internal_wsgi_application()
-
     def handle(self, *args, **options):
-        if not settings.DEBUG and not settings.ALLOWED_HOSTS:
-            raise CommandError('You must set settings.ALLOWED_HOSTS if DEBUG is False.')
-
-        self.use_ipv6 = options['use_ipv6']
-        if self.use_ipv6 and not socket.has_ipv6:
-            raise CommandError('Your Python does not support IPv6.')
-        self._raw_ipv6 = False
         if not options['addrport']:
             self.addr = ''
             self.port = self.default_port
@@ -82,16 +59,8 @@ class Command(BaseCommand):
             self.addr, _ipv4, _ipv6, _fqdn, self.port = m.groups()
             if not self.port.isdigit():
                 raise CommandError("%r is not a valid port number." % self.port)
-            if self.addr:
-                if _ipv6:
-                    self.addr = self.addr[1:-1]
-                    self.use_ipv6 = True
-                    self._raw_ipv6 = True
-                elif self.use_ipv6 and not _fqdn:
-                    raise CommandError('"%s" is not a valid IPv6 address.' % self.addr)
         if not self.addr:
-            self.addr = self.default_addr_ipv6 if self.use_ipv6 else self.default_addr
-            self._raw_ipv6 = self.use_ipv6
+            self.addr = self.default_addr
         self.run(**options)
 
     def run(self, **options):
@@ -107,8 +76,8 @@ class Command(BaseCommand):
         # If an exception was silenced in ManagementUtility.execute in order
         # to be raised in the child process, raise it now.
         autoreload.raise_last_exception()
+        max_workers = options.get('max_workers', 5)
 
-        threading = options['use_threading']
         # 'shutdown_message' is a stealth option.
         shutdown_message = options.get('shutdown_message', '')
         quit_command = 'CTRL-BREAK' if sys.platform == 'win32' else 'CONTROL-C'
@@ -121,22 +90,21 @@ class Command(BaseCommand):
         now = datetime.now().strftime('%B %d, %Y - %X')
         self.stdout.write(now)
         self.stdout.write((
-            "Django version %(version)s, using settings %(settings)r\n"
-            "Starting development server at %(protocol)s://%(addr)s:%(port)s/\n"
-            "Quit the server with %(quit_command)s.\n"
-        ) % {
-            "version": self.get_version(),
-            "settings": settings.SETTINGS_MODULE,
-            "protocol": self.protocol,
-            "addr": '[%s]' % self.addr if self._raw_ipv6 else self.addr,
-            "port": self.port,
-            "quit_command": quit_command,
-        })
+                              "Django version %(version)s, using settings %(settings)r\n"
+                              "Starting development server at %(protocol)s:%(addr)s:%(port)s\n"
+                              "Quit the server with %(quit_command)s.\n"
+                          ) % {
+                              "version": self.get_version(),
+                              "settings": settings.SETTINGS_MODULE,
+                              "protocol": self.protocol,
+                              "addr": '%s' % self.addr,
+                              "port": self.port,
+                              "quit_command": quit_command,
+                          })
 
         try:
-            handler = self.get_handler(*args, **options)
-            run(self.addr, int(self.port), handler,
-                ipv6=self.use_ipv6, threading=threading, server_cls=self.server_cls)
+            server = self.server_cls(max_workers=max_workers)
+            server.run(address=self.addr, port=self.port)
         except OSError as e:
             # Use helpful error messages instead of ugly tracebacks.
             ERRORS = {
@@ -155,7 +123,3 @@ class Command(BaseCommand):
             if shutdown_message:
                 self.stdout.write(shutdown_message)
             sys.exit(0)
-
-
-# Kept for backward compatibility
-BaseRunserverCommand = Command
